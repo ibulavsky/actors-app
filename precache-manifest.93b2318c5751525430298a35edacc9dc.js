8f53505b8c117018a1c28e6b0b584fd2adcc77eb
ibulavsky <ibulavsky@gmail.com>
2020-02-10 10:39:52 +0300

    Updates

---
+++ precache-manifest.93b2318c5751525430298a35edacc9dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f72ceea2e8955f3c40fdc388a67f4de5",
+    "url": "/actors-app/index.html"
+  },
+  {
+    "revision": "bd31de4cb68170832b24",
+    "url": "/actors-app/static/css/2.1e105a3b.chunk.css"
+  },
+  {
+    "revision": "b8ee3b6d26ef882514c4",
+    "url": "/actors-app/static/css/main.894b211a.chunk.css"
+  },
+  {
+    "revision": "bd31de4cb68170832b24",
+    "url": "/actors-app/static/js/2.1b59965c.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/actors-app/static/js/2.1b59965c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8ee3b6d26ef882514c4",
+    "url": "/actors-app/static/js/main.edef7a61.chunk.js"
+  },
+  {
+    "revision": "110bc78eed2ffc787069",
+    "url": "/actors-app/static/js/runtime-main.0c42489f.js"
+  }
+]);
