f5a0cbed301bce020d359866eb4eb7353a2d75f1
ibulavsky <ibulavsky@gmail.com>
2020-02-10 10:23:11 +0300

    Added gh-pages.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React from 'react';
 import {BrowserRouter} from "react-router-dom"
 import {Provider} from "react-redux"
 import store from "./0-Main/1-main-bll/store"
-
 import Main from "./0-Main/0-main-ui/Main"
 import 'antd/dist/antd.css'
 import './App.css';
 
-function App() {
+export default function App() {
     return (
         <div className="App">
-            <BrowserRouter>
+            <BrowserRouter  history={history} basename={process.env.PUBLIC_URL}>
                 <Provider store={store}>
                     <Main/>
                 </Provider>
@@ -18,5 +17,3 @@ function App() {
         </div>
     );
 }
-
-export default App;
